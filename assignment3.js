577471215e5b702c66c1c09ce026678a32d0f594
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-08 01:22:05 +0600

    problem2done

--- assignment3.js
+++ assignment3.js
@@ -1,6 +1,22 @@
-//  problem -01
+//  Problem -01 ( Divide the Asset )
 var area = 5550;
 if(area > 0 && area<=10**9 ){
     var result = area / 2;
     console.log(result)
 }
+
+// Problem -02 ( Cycle or Laptop )
+var money = 11000;
+if(money >= 25000){
+    console.log('Laptop')
+}
+else if(money >= 10000){
+    console.log('Cycle')
+}
+else{
+    console.log('Chocolate')
+}
+
+
+// Problem -03 ( Medicine Planner )
+var lastDay = 11 ;
